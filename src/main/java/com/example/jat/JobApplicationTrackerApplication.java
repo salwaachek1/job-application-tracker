a994bc9f96801c58661f9c7a10ad17d2f4ca6115
Salwa Achek <salwaachek1@gmail.com>
2025-12-01 13:47:25 +0100

    chore: initial Spring Boot skeleton with Docker and Postgres

---
+++ src/main/java/com/example/jat/JobApplicationTrackerApplication.java
@@ -0,0 +1,11 @@
+package com.example.jat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplicationTrackerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JobApplicationTrackerApplication.class, args);
+    }
+}
